6dcaf7cfff109090be342c2e72945b59f012357b
Vinson Wong <vinson.wong93@gmail.com>
2023-02-28 18:47:50 -0500

    implement restaurantDetails component with index.js for menu list items and Header.js for menu header

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
     <View style={styles.container}>
       {/* <HomeScreen/> */}
       <RestaurantDetailsPage />
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </View>
   );
 }
--- src/screens/RestaurantDetailsScreen/index.js
+++ src/screens/RestaurantDetailsScreen/index.js
@@ -2,54 +2,29 @@ import { Text, View, Image, FlatList, StyleSheet } from 'react-native';
 import React from 'react';
 import restaurants from '../../../assets/data/restaurants.json';
 import { Ionicons } from '@expo/vector-icons';
-const restaurant = restaurants[0];
+import MenuListItem from '../../components/MenuListItem';
+import Header from "./Header";
+import styles from './styles';
 
+const restaurant = restaurants[0];
 
 const RestaurantDetailsPage = () => {
   return (
     <View style={styles.page}>
-      <Image source={{ uri: restaurant.image }} style={styles.image} resizeMode="cover" />
+      <FlatList 
+        ListHeaderComponent={() => <Header restaurant={restaurant} />}
+        data={restaurant.dishes}
+        renderItem={({item}) => <MenuListItem dish={item} />}
+      />
         <Ionicons
           name="arrow-back-circle"
           size={45} 
           color="white"
           style={styles.iconContainer}
         />
-      <View style={styles.container}>
-        <Text style={styles.title}>{restaurant.name}</Text>
-        <Text style={styles.subtitle}>${restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} minutes</Text>
-      </View>
     </View>
   );
 };
 
 export default RestaurantDetailsPage;
 
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-  },
-  iconContainer: {
-
-    position: 'absolute',
-    top: 40,
-    left: 10,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 5 / 3,
-  },
-  title: {
-    fontSize: 35,
-    fontWeight: '600',
-    marginVertical: 10,
-
-  },
-  subtitle: {
-    color: '#525252',
-    fontSize: 15,
-  },
-  container: {
-    margin: 10,
-  }
-});
