04b4c4cad8e837573bf217134e12c32389c3c432
Vinson Wong <vinson.wong93@gmail.com>
2023-02-28 11:54:56 -0500

    create restaurantItem component and implement functional component with appropriate styling

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import RestaurantItem from './src/components/RestaurantItem';
+import restaurants from './assets/data/restaurants.json';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Restaurant Item */}
+      <RestaurantItem restaurant={restaurants[0]}/>
+      <RestaurantItem restaurant={restaurants[1]}/>
+      <RestaurantItem restaurant={restaurants[2]}/>
+
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +24,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
+
 });
