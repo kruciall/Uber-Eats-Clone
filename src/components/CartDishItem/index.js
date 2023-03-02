dd929e6e9d6058c52e13d8723146b2afa6b28a33
Vinson Wong <vinson.wong93@gmail.com>
2023-03-02 12:47:25 -0500

    fix styling for CartDishItem component

--- src/components/CartDishItem/index.js
+++ src/components/CartDishItem/index.js
@@ -6,13 +6,16 @@ import React from 'react';
 
 const CartDishItem = ({ cartDish }) => {
   return (
-    <View style={styles.row}>
+    <View>
+      <View style={styles.row}>
       <View style={styles.quantityContainer}>
         <Text>1</Text>
       </View>
       <Text style={styles.itemName}>{cartDish.name}</Text>
       <Text style={styles.quantityPrice}>${cartDish.price}</Text>
     </View>
+    <View style={styles.separator} />
+      </View>
   );
 };
 
@@ -20,18 +23,36 @@ export default CartDishItem;
 
 
 const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    width: '100%',
+    paddingVertical: 50,
+    padding: 10,
+  },
+  description: {
+    color: '#696969'
+  },
   separator: {
     height: 1,
     backgroundColor: 'lightgrey',
-    marginVertical: 20,
+    marginVertical: 5,
+    margin: 20,
   },
   row: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 22,
+    marginTop: 12,
     paddingHorizontal: 12,
+    margin: 20,
+  },
+  quantity: {
+    fontSize: 25,
+    marginHorizontal: 20,
   },
 
+  quantityPrice: {
+    marginLeft: 'auto',
+  },
   quantityContainer: {
     backgroundColor: 'lightgrey',
     paddingHorizontal: 5,
